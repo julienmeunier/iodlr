08d22fc9bcb4c825a7038855ead4f6882dcbeccd
Julien Meunier <julien.meunier@nokia.com>
2022-11-17 16:39:26 +0200

    large_page-c: add dev/debug flags
    Signed-off-by: Julien Meunier <julien.meunier@nokia.com>

--- large_page-c/cflags.mk
+++ large_page-c/cflags.mk
@@ -1,1 +1,1 @@
-CFLAGS_COMMON=-O3 -D_FORTIFY_SOURCE=2 -z noexecstack -z relro -z now -fstack-protector -Wformat -Wformat-security -Wall
+CFLAGS_COMMON=-O3 -D_FORTIFY_SOURCE=2 -z noexecstack -z relro -z now  -fno-stack-protector -Wformat -Wformat-security -Wall -ggdb3 -fPIC -Wl,-whole-archive
